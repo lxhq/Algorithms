d5f20c2e03f1abc235b2180aad61eaa1513cc479
lxhq <lxheqiao@163.com>
2019-09-15 21:28:56 -0500

    Create Kadane.java

---
+++ Kadane.java
@@ -0,0 +1,11 @@
+//Kadane's algorithm for finding the maxmium subarray in array
+public int kadane(int[] arr) {
+    int maxSoFar = 0;
+    int[] maxEndHere = new int[arr.lenght];
+    max[0] = arr[0];
+    for (int i = 1; i < n; i++) {
+        maxEndHere[i] = Math.max(arr[i], maxEndHere[i - 1] + arr[i]);
+        maxSoFar = Math.max(maxSoFar, maxEndHere[i]);
+    }
+    return maxSoFar;
+}
