efbafd0852ab8416c8a487d1ff71fa0530524a2a
lxhq <lxheqiao@163.com>
2019-09-15 19:31:47 -0500

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -1,11 +1,13 @@
-//This is a undirected graph with dfs algorithm that can find the bridge and cycles in the graph 
+//This is a directed graph with dfs algorithm that can find the bridge and strongly connected components in the graph 
+//The node itself is counted as one of those components
 //(Tarjan's algorithm)
 //https://www.youtube.com/watch?v=TyWtx7q2D7Y
 //https://www.youtube.com/watch?v=aZXi1unBdJA
 
 class UnDirGraph {
+class Graph {
     List<Integer>[] graph;
-    public UnDirGraph(int size) {
+    public Graph(int size) {
         graph = new ArrayList[size];
         for (int i = 0; i < size; i++) {
             graph[i] = new ArrayList<>();
@@ -14,7 +16,6 @@ class UnDirGraph {
 
     public void addEdge(int from, int to) {
         graph[from].add(to);
-        graph[to].add(from);
     }
 
     public List<Integer> adj(int node) {
@@ -27,7 +28,7 @@ class UnDirGraph {
 }
 
 class DFS {
-    UnDirGraph graph;
+    Graph graph;
     List<int[]> bridges; 
     List<int[]> edgesInCycles;
     Deque<Integer> stack;
@@ -36,8 +37,8 @@ class DFS {
     int[] low;
     int[] edges;
     int id;
-    int cycles;
-    public DFS(UnDirGraph graph) {
+    int sccCount;
+    public DFS(Graph graph) {
         this.graph = graph;
         ids = new int[graph.size()]; Arrays.fill(ids, -1);
         low = new int[graph.size()];
@@ -47,13 +48,17 @@ class DFS {
         stack = new ArrayDeque<>();
         edgesInCycles = new ArrayList<>();
         id = 0;
-        dfs(0);
+        for (int i = 0; i < graph.size(); i++) {
+            if (ids[i] == -1) {
+                dfs(0);
+            }
+        }
     }
 
     private void dfs(int at) {
-        ids[at] = low[at] = id++;
         stack.push(at);
         onStack[at] = true;
+        ids[at] = low[at] = id++;
         for (int to : graph.adj(at)) {
             if (to == edges[at]) continue;
             if (ids[to] == -1) {
@@ -63,21 +68,21 @@ class DFS {
                 if (ids[at] < low[to]) {
                     bridges.add(new int[]{at, to});
                 }
-            } else {
+            } 
+            if (onStack[to]) {
                 low[at] = Math.min(low[at], ids[to]);
             }
         }
         if (ids[at] == low[at]) {
             int node = stack.pop();
-            if (at == node) return;
-            edgesInCycles.add(new int[]{at, node});
+            edgesInCycles.add(new int[]{node, at});
             while (node != at) {
                 onStack[node] = false;
                 low[node] = ids[at];
                 edgesInCycles.add(new int[]{edges[node], node});
                 node = edges[node];
             }
-            cycles++;
+            sccCount++;
         }
     }
 }
