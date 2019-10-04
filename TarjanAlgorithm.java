b49156324a3067c5df7682b26ea1516609e42c7b
lxhq <lxheqiao@163.com>
2019-10-03 20:53:23 -0500

    Update and rename Graph.java to TarjanAlgorithm.java

--- TarjanAlgorithm.java
+++ TarjanAlgorithm.java
@@ -13,76 +13,50 @@ class Graph {
             graph[i] = new ArrayList<>();
         }
     }
-
+        
     public void addEdge(int from, int to) {
         graph[from].add(to);
+        graph[to].add(from);
     }
-
+        
     public List<Integer> adj(int node) {
         return graph[node];
     }
-
+        
     public int size() {
         return graph.length;
     }
 }
-
+    
 class DFS {
     Graph graph;
-    List<int[]> bridges; 
-    List<int[]> edgesInCycles;
-    Deque<Integer> stack;
-    boolean[] onStack;
     int[] ids;
     int[] low;
     int[] edges;
     int id;
-    int sccCount;
+    List<List<Integer>> bridges;
     public DFS(Graph graph) {
         this.graph = graph;
-        ids = new int[graph.size()]; Arrays.fill(ids, -1);
+        bridges = new ArrayList<>();
+        ids = new int[graph.size()];
         low = new int[graph.size()];
         edges = new int[graph.size()];
-        onStack = new boolean[graph.size()];
-        bridges = new ArrayList<>();
-        stack = new ArrayDeque<>();
-        edgesInCycles = new ArrayList<>();
         id = 0;
-        for (int i = 0; i < graph.size(); i++) {
-            if (ids[i] == -1) {
-                dfs(i);
-            }
-        }
-    }
-
+        Arrays.fill(ids, -1);
+        dfs(0);
+    }  
     private void dfs(int at) {
-        stack.push(at);
-        onStack[at] = true;
         ids[at] = low[at] = id++;
         for (int to : graph.adj(at)) {
-            if (to == edges[at]) continue;
+           if (to == edges[at]) continue;
             if (ids[to] == -1) {
                 edges[to] = at;
                 dfs(to);
                 low[at] = Math.min(low[at], low[to]);
-                if (ids[at] < low[to]) {
-                    bridges.add(new int[]{at, to});
-                }
-            } 
-            if (onStack[to]) {
+                if (ids[at] < low[to]) bridges.add(new ArrayList<>(Arrays.asList(at, to)));
+            } else {
                 low[at] = Math.min(low[at], ids[to]);
             }
-        }
-        if (ids[at] == low[at]) {
-            int node = stack.pop();
-            edgesInCycles.add(new int[]{node, at});
-            while (node != at) {
-                onStack[node] = false;
-                low[node] = ids[at];
-                edgesInCycles.add(new int[]{edges[node], node});
-                node = stack.pop();
-            }
-            sccCount++;
         }
     }
 }
